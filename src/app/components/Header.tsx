d5976d50ceb9fd7cc016f8d15637f315be5ea682
Matheus Fillipe <matheusfillipe@hotmail.com>
2025-03-29 23:06:54 -0300

    Update Header component with external links and enhance SearchInput focus styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -12,21 +12,26 @@ export default function Header() {
       <nav className='flex gap-6 font-semibold'>
         <a href='#'>Home</a>
         <a href='#'>Explore</a>
-        <a href='#'>Compre Online</a>
+        <a href='https://lordperfumaria.com.br/' target='_blank'>
+          Compre Online
+        </a>
         <a href='#'>Quem Somos</a>
       </nav>
 
       <div className='flex gap-4'>
-        <a href='#'>
+        <a href='https://www.instagram.com/lordperfumaria/' target='_blank'>
           <Image src='/Instagram.svg' alt='Logo' width={24} height={24} />
         </a>
-        <a href='#'>
+        <a href='https://www.tiktok.com/@lordperfumaria' target='_blank'>
           <Image src='/TikTok.svg' alt='Logo' width={24} height={24} />
         </a>
-        <a href='#'>
+        <a href='https://www.youtube.com/lordperfumaria' target='_blank'>
           <Image src='/YouTube.svg' alt='Logo' width={24} height={24} />
         </a>
-        <a href='#'>
+        <a
+          href='https://api.whatsapp.com/send/?phone=556133284888&text&type=phone_number&app_absent=0'
+          target='_blank'
+        >
           <Image src='/WhatsApp.svg' alt='Logo' width={24} height={24} />
         </a>
       </div>
