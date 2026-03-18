da6dab9ba75c01a0eaa53cac6fcca2ad9851850a
Matheus Fillipe <matheusfillipe@hotmail.com>
2026-03-18 16:38:39 -0300

    refactor: Update Next.js image configuration to use `remotePatterns` instead of `domains`.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['lordperfumariablog.wordpress.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lordperfumariablog.wordpress.com',
+      },
+    ],
   },
 }
 
